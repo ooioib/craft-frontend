483341ad794727a0635535e8e09b40c8eba82215
rin <krin02@naver.com>
2025-05-29 19:15:37 +0900

    feat : 주문 및 재고 조회 화면 설계

---
+++ src/app/(dashboard)/order/new/page.js
@@ -0,0 +1,101 @@
+"use client";
+import {
+  Box,
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+import { useEffect, useState } from "react";
+
+export default function NewOrderPage() {
+  const [dueDate, setDueDate] = useState();
+  const [items, setItems] = useState([{}]);
+
+  const addOrderHandle = function () {
+    fetch("http://127.0.0.1:8080/api/order/new", {
+      method: "post",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ dueDate, items }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        window.alert(data.message);
+      });
+  };
+
+  return (
+    <Box sx={{ p: 2 }}>
+      <Paper sx={{ p: 2 }}>
+        <Typography variant="h5">주문 등록</Typography>
+      </Paper>
+      <Box sx={{ p: 2 }}>
+        <TextField
+          type="date"
+          size="small"
+          onChange={(evt) => {
+            setDueDate(evt.target.value);
+          }}
+        />
+      </Box>
+      <Box>
+        <Box sx={{ mt: 2 }}>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              setItems([...items, {}]);
+            }}
+          >
+            + 항목 추가
+          </Button>
+          <Button variant="outlined" onClick={addOrderHandle}>
+            {" "}
+            주문 등록
+          </Button>
+        </Box>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>제품 코드</TableCell>
+              <TableCell>수량</TableCell>
+              <TableCell align="center">삭제</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {items.map((e, idx) => (
+              <TableRow key={idx}>
+                <TableCell>
+                  <TextField
+                    size="small"
+                    onChange={(evt) => {
+                      items[idx].productId = evt.target.value;
+                    }}
+                  />
+                </TableCell>
+                <TableCell>
+                  <TextField
+                    size="small"
+                    onChange={(evt) => {
+                      items[idx].quantity = evt.target.value;
+                    }}
+                  />
+                </TableCell>
+                <TableCell>
+                  <Button onClick={() => {}}>삭제</Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Box>
+    </Box>
+  );
+}
